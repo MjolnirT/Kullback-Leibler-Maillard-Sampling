f1e792c22485d19528d25a88da0df14e25bde0a5
MjolnirT <Tansuc@live.com>
2023-05-11 15:18:25 -0700

    Add regret plots from multiple simulations (including confidence interval)

--- main.py
+++ main.py
@@ -7,61 +7,62 @@ from simulate import simulate
 
 
 if __name__ == '__main__':
-    reward_probabilities = [0.1] * 5 + [0.5] * 10 + [0.8] * 10 + [0.97] * 10 + [0.98] * 5 + [0.999]
+    reward_probabilities = [0.1] + [0.2]
+    # reward_probabilities = [0.1] * 5 + [0.5] * 10 + [0.8] * 10 + [0.97] * 10 + [0.98] * 5 + [0.999]
     # reward_probabilities = [0.1]*5 + [0.5]*10 + [0.6]
 
     print_flag = True
     message(f'reward_probabilities: {reward_probabilities}', print_flag=True)
-    n_rounds = 100
-    n_arms = len(reward_probabilities)
 
-    # doing simulation for 100 times
+    # to pick the best configuration for MS+, doing a grid search from 100 simulations
+    opt_config = SearchOptConfig(reward_probabilities, 100)
 
-    n_simulations = 100
-    opt_config = SearchOptConfig(reward_probabilities, n_rounds)
+    # set algorithms and their parameters
     variance = 1/4
-
-    algorithms = [(BernoulliTS, [n_arms, n_rounds]),
-                  (KLMS, [n_arms, n_rounds]),
-                  (KLMSJefferysPrior, [n_arms, n_rounds]),
-                  (MS, [n_arms, n_rounds, 1/4]),
-                  (MSPlus, [n_arms, n_rounds] + list(opt_config) + [1/4])]
+    T_timespan = 100
+    n_arms = len(reward_probabilities)
+    n_simulations = 100
+    algorithms = [(BernoulliTS, [n_arms, T_timespan]),
+                  (KLMS, [n_arms, T_timespan]),
+                  (KLMSJefferysPrior, [n_arms, T_timespan]),
+                  (MS, [n_arms, T_timespan, 1 / 4]),
+                  (MSPlus, [n_arms, T_timespan] + list(opt_config) + [1 / 4])]
     algorithms_name = ['BernoulliTS', 'KLMS', 'KLMS+JefferysPrior', 'MS', 'MS+']
 
-    avg_reward = np.zeros(shape=[n_simulations, len(algorithms)])
+    evl_rewards = np.zeros(shape=[n_simulations, len(algorithms)])
+    regrets = np.zeros(shape=[n_simulations, len(algorithms), T_timespan])
     for i in range(n_simulations):
         if i % 10 == 0:
-            message(f'Simulation {i}', print_flag=print_flag)
+            message(f'Simulation {i} / {n_simulations}', print_flag=print_flag)
         for alg_idx, (algorithm, args) in enumerate(algorithms):
             model = algorithm(*args)
-            _, rewards, best_reward = simulate(reward_probabilities, n_rounds, model)
-            regret = np.array(best_reward) - np.array(rewards)
+            _, rewards, best_reward = simulate(reward_probabilities, T_timespan, model)
+            regrets[i, alg_idx] = np.array(best_reward) - np.array(rewards)
 
             arm_prob = model.get_arm_prob()
-            avg_reward[i, alg_idx] = np.array(reward_probabilities).dot(arm_prob)
+            evl_rewards[i, alg_idx] = np.array(reward_probabilities).dot(arm_prob)
 
-    plot_density(avg_reward, 'Average Reward Comparison', label=algorithms_name)
+    plot_density(evl_rewards, 'Evaluation Reward Comparison', label=algorithms_name)
 
-    message('Running BernoulliTS', print_flag=print_flag)
-    BernoulliTS = BernoulliTS(n_arms, n_rounds)
-    _, rewards, best_reward = simulate(reward_probabilities, n_rounds, BernoulliTS)
-    B_TS_regret = np.array(best_reward) - np.array(rewards)
+    ref_alg = "BernoulliTS"
 
-    message('Running KLMS', print_flag=print_flag)
-    KLMS = KLMS(n_arms, explore_weight=1.0, n_rounds=n_rounds)
-    _, rewards, best_reward = simulate(reward_probabilities, n_rounds, KLMS)
-    MS_regret = np.array(best_reward) - np.array(rewards)
-    message(f"Probability of arms: {KLMS.get_arm_prob()}", print_flag=print_flag)
+    # plot cumulative regret vs time step
+    cum_regrets = np.cumsum(regrets, axis=2)
+    plot_regrets(cum_regrets,
+                 ci=0.95,
+                 y_label='Cumulative regret',
+                 title='Regret Comparison',
+                 label=algorithms_name,
+                 ref_alg=ref_alg)
 
-    message('Running KLMS with JeffreysPrior', print_flag=print_flag)
-    KLMSJefferysPrior = KLMSJefferysPrior(n_arms, explore_weight=1.0, n_rounds=n_rounds)
-    _, rewards, best_reward = simulate(reward_probabilities, n_rounds, KLMSJefferysPrior)
-    MS_Jeff_regret_2 = np.array(best_reward) - np.array(rewards)
-    message(f"Probability of arms: {KLMSJefferysPrior.get_arm_prob()}", print_flag=print_flag)
+    # plot average regret vs time step
+    avg_regret = cum_regrets / np.arange(1, T_timespan + 1)
+    plot_regrets(avg_regret,
+                 ci=0.95,
+                 y_label='Average regret',
+                 title='Regret Comparison',
+                 label=algorithms_name,
+                 ref_alg=ref_alg)
 
-    label = ['Bernoulli TS', 'KLMS', 'KLMS with Jeffreys Prior']
-    regret_list = [B_TS_regret, MS_regret, MS_Jeff_regret_2]
-    arm_prob_list = [BernoulliTS.get_arm_prob(), KLMS.get_arm_prob(), KLMSJefferysPrior.get_arm_prob()]
-
-    plot_regrets(regret_list, 'Regret Comparison', label)
-    plot_arm_prob(arm_prob_list, 'Arm Probability Comparison', label)
+    # plot arm probability
+    # plot_arm_prob(arm_prob_list, 'Arm Probability Comparison', label)
--- utility.py
+++ utility.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import gaussian_kde
+from matplotlib.cm import get_cmap
 
 
 def plot_regret(regret, title=None, label=None):
@@ -18,21 +19,36 @@ def plot_regret(regret, title=None, label=None):
     plt.show()
 
 
-def plot_regrets(regrest_list, title=None, label=None):
-    for idx, regret in enumerate(regrest_list):
-        cum_regret = np.cumsum(np.array(regret))
-        plt.plot(range(len(cum_regret)), cum_regret, label=label[idx])
-    plt.xlabel('Round')
-    plt.ylabel('Cumulative regret')
-    plt.title(title)
-    plt.legend()
-    plt.show()
+def plot_regrets(regrets, ci=0.95, y_label=None, title=None, label=None, ref_alg=None):
+    n_simulations, num_algorithm, T_timespan = regrets.shape
+    average_regret = regrets.mean(axis=0)
 
-    for idx, regret in enumerate(regrest_list):
-        cum_regret = np.cumsum(np.array(regret))
-        plt.plot(range(len(cum_regret)), cum_regret / np.arange(1, len(cum_regret) + 1), label=label[idx])
-    plt.xlabel('Round')
-    plt.ylabel('Average Regret')
+    # calculate the confidence interval
+    confidence_level = ci
+    lower_bound = np.quantile(regrets, (1 - confidence_level) / 2, axis=0)
+    upper_bound = np.quantile(regrets, 1 - (1 - confidence_level) / 2, axis=0)
+
+    # Get a colormap with the number of algorithms
+    cmap = get_cmap('tab10', num_algorithm)
+
+    for idx_alg in range(num_algorithm):
+        plt.plot(range(1, T_timespan + 1), average_regret[idx_alg, :], label=label[idx_alg])
+
+        if label[idx_alg] in ref_alg:
+            plt.fill_between(range(1, T_timespan + 1),
+                             lower_bound[idx_alg, :],
+                             upper_bound[idx_alg, :],
+                             color=cmap(idx_alg),
+                             alpha=0.2)
+            # Add label to the confidence interval
+            plt.text(
+                T_timespan, upper_bound[idx_alg, -1],
+                f'{label[idx_alg]}:{confidence_level}% CI', ha='right', va='bottom',
+                color=cmap(idx_alg), fontsize=8
+            )
+
+    plt.xlabel('time step')
+    plt.ylabel(y_label)
     plt.title(title)
     plt.legend()
     plt.show()
@@ -51,9 +67,11 @@ def plot_arm_prob(arm_prob_list, title=None, label=None):
 def plot_density(rewards, title=None, label=None):
     fig, ax = plt.subplots()
 
+    n_simulations, num_algorithm = rewards.shape
+
     # Iterate over the columns of the data and plot the density function for each
-    for i in range(rewards.shape[1]):
-        alg_reward = rewards[:, i]
+    for idx_alg in range(num_algorithm):
+        alg_reward = rewards[:, idx_alg]
 
         # Fit a probability distribution to the column data
         dist = gaussian_kde(alg_reward)
@@ -62,12 +80,12 @@ def plot_density(rewards, title=None, label=None):
         x = np.linspace(alg_reward.min(), alg_reward.max(), 100)
         y = dist.pdf(x)
 
-        ax.plot(x, y, label=label[i])
+        ax.plot(x, y, label=label[idx_alg])
 
     # Add a legend and labels to the plot
     ax.legend()
-    ax.set_xlabel('Cumulative reward')
-    ax.set_ylabel('Density')
+    ax.set_xlabel('Evaluation reward')
+    ax.set_ylabel('Frequency')
     ax.set_title(title)
 
     # Show the plot
