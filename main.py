f0e92e1b68e7b43a6fad1a3aa89dade5f3f8ec82
MjolnirT <Tansuc@live.com>
2024-10-04 14:43:06 -0700

    clean up main entry point and simulation file

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import os.path
+import os
 import sys
 import pickle
 import time
-from simulation import simulate_single_simulation
 from multiprocessing import Pool, cpu_count, Manager
+from simulation import simulate_single_simulation
 from utility_functions.generate_plots import generate_plots
-from utility_functions.utility import *
+from utility_functions.utility import message
 from utility_functions.utility_io import check_folder_exist, get_filename, read_algorithms
 from global_config import DATA_DIR, PLOT_DIR, LOG_FLAG
 
 
-if __name__ == '__main__':
+def main():
     start_time = time.time()
-
+    check_folder_exist(DATA_DIR)
+    check_folder_exist(PLOT_DIR)
+    # Read the config file
     config_filepath = sys.argv[1]
     environment, algorithms = read_algorithms(config_filepath, print_flag=LOG_FLAG)
     simulations_per_round = algorithms["BernoulliTS"]["params"]["simulation_rounds"]
@@ -23,45 +25,40 @@ if __name__ == '__main__':
 
     algorithms_name = list(algorithms.keys())
 
-    # parallel simulation process
-    # Use a maximum of 20 processes or the available CPU threads, whichever is smaller
+    # Parallel simulation process
     message('--- Start parallel simulation process ---', print_flag=LOG_FLAG)
     num_processes = min(16, cpu_count())
     message(f'Using CPUs: {num_processes}', print_flag=LOG_FLAG)
-    pool = Pool(processes=num_processes)
+    
+    with Pool(processes=num_processes) as pool:
+        # Create a shared counter and a lock
+        manager = Manager()
+        counter = manager.Value('i', 0)
+        lock = manager.Lock()
 
-    # Create a shared counter and a lock.
-    manager = Manager()
-    counter = manager.Value('i', 0)
-    lock = manager.Lock()
-
-    # Start the pool with the modified function.
-    results = pool.starmap(simulate_single_simulation,
-                           [(i, counter, lock, algorithms, algorithms_name, n_simulations, env_reward) for i
-                            in range(n_simulations)])
-
+        # Start the pool with the modified function
+        results = pool.starmap(simulate_single_simulation,
+                               [(i, counter, lock, algorithms, algorithms_name, n_simulations, env_reward) 
+                                for i in range(n_simulations)])
     print(f"All {n_simulations} simulations completed.")
 
-    pool.close()
-    pool.join()
-
-    simulation_filename = get_filename(T_timespan, n_simulations, test_case,
-                                       simulations_per_round, is_simulation=True)
-    simulation_filename = os.path.join(DATA_DIR, simulation_filename)
-    check_folder_exist(DATA_DIR)
-    with open(simulation_filename, 'wb') as file:
+    # Save the results
+    simulation_filepath = os.path.join(DATA_DIR, get_filename(T_timespan, n_simulations, test_case,
+                                                              simulations_per_round, is_simulation=True))
+    with open(simulation_filepath, 'wb') as file:
         pickle.dump(results, file)
-    file.close()
 
-    # print out execution time
-    message(f'Total time elapsed {time.time() - start_time}', LOG_FLAG)
-    exclude_alg = ['KL-MS+JefferysPrior', 'MS', 'MS+']
-    check_folder_exist(PLOT_DIR)
-    generate_plots(simulation_filename,
+    # Print out execution time
+    elapsed_time = time.time() - start_time
+    message(f'Total time elapsed {elapsed_time}', LOG_FLAG)
+    generate_plots(simulation_filepath,
                    env_reward,
                    algorithms_name,
                    PLOT_DIR,
                    ref_alg='BernoulliTS',
                    exclude_alg=None)
-    message(f'time elapsed {time.time() - start_time}', LOG_FLAG)
-    message(f'Simulation file path: {simulation_filename}', LOG_FLAG)
+    message(f'time elapsed {elapsed_time}', LOG_FLAG)
+    message(f'Simulation file path: {simulation_filepath}', LOG_FLAG)
+
+if __name__ == '__main__':
+    main()
--- simulation.py
+++ simulation.py
@@ -2,33 +2,33 @@ import time
 import numpy as np
 
 
-def simulate_one_alg(env_reward, n_arms, n_rounds, algorithm, output_all_arm_prob=False):
+def simulate_one_alg(env_reward, 
+                     n_arms, 
+                     n_rounds, 
+                     algorithm, 
+                     output_all_arm_prob=False):
+    
     rewards = []
     selected_arms = []
-    # using pseudo reward to calculate the regret
     best_reward = np.max(env_reward) * np.ones(n_rounds)
-    arm_probs = np.zeros(shape=[n_rounds, n_arms])
+    arm_probs = np.zeros((n_rounds, n_arms))
 
     for t in range(n_rounds):
-        chosen_arm = None
         # In the first n_arms rounds, play each arm once
         if t < n_arms:
             chosen_arm = t
-
-        # After the first n_arms rounds, use the algorithm to select an arm
-        if t >= n_arms:
+        else:
             chosen_arm = algorithm.select_arm()
-
-        # sample a return_reward from a Bernoulli distribution
-        # return_reward = np.random.binomial(1, reward_probabilities[chosen_arm])
+        
+        # Sample a return_reward from the environment
         return_reward = env_reward[chosen_arm]
         algorithm.update(chosen_arm, return_reward)
 
-        # record the results
+        # Record results
         selected_arms.append(chosen_arm)
         rewards.append(return_reward)
 
-        # record the probability of each arm
+        # Record the probability of each arm
         if output_all_arm_prob:
             arm_probs[t] = algorithm.get_arm_prob()
 
@@ -37,35 +37,39 @@ def simulate_one_alg(env_reward, n_arms, n_rounds, algorithm, output_all_arm_pro
     return selected_arms, rewards, best_reward, arm_probs
 
 
-def simulate_single_simulation(simulation_idx, counter, lock, algorithms, algorithms_name, n_simulations, env_reward):
-    first_alg_key = list(algorithms.keys())[0]
+def simulate_single_simulation(simulation_idx, 
+                               counter, 
+                               lock, 
+                               algorithms, 
+                               algorithms_name, 
+                               n_simulations, 
+                               env_reward):
+
+    first_alg_key = next(iter(algorithms))
     T_timespan = algorithms[first_alg_key]['params']['T_timespan']
     n_arms = algorithms[first_alg_key]['params']['n_arms']
 
-    selected_arm_all = np.zeros(shape=[len(algorithms), T_timespan])
-    regrets_all = np.zeros(shape=[len(algorithms), T_timespan])
-    arm_probs_all = np.zeros(shape=[len(algorithms), T_timespan, n_arms])
-    expected_rewards_all = np.zeros(shape=[len(algorithms)])
+    selected_arm_all = np.zeros((len(algorithms), T_timespan))
+    regrets_all = np.zeros((len(algorithms), T_timespan))
+    arm_probs_all = np.zeros((len(algorithms), T_timespan, n_arms))
+    expected_rewards_all = np.zeros(len(algorithms))
+    time_cost = np.zeros(len(algorithms))
 
-    time_cost = np.zeros(shape=[len(algorithms)])
-    for alg_idx, algorithm in enumerate(algorithms):
-        # model = algorithm(*args)
+    for alg_idx, (_, algorithm_info) in enumerate(algorithms.items()):
         start_time = time.time()
-        model = algorithms[algorithm]['model'](**algorithms[algorithm]['params'])
+        model = algorithm_info['model'](**algorithm_info['params'])
         model.set_name(algorithms_name[alg_idx])
-        selected_arms, rewards, best_reward, arm_prob = simulate_one_alg(env_reward,
-                                                                         n_arms,
-                                                                         T_timespan,
-                                                                         model,
-                                                                         output_all_arm_prob=True)
+
+        selected_arms, rewards, best_reward, arm_prob = simulate_one_alg(
+            env_reward, n_arms, T_timespan, model, output_all_arm_prob=True
+        )
 
         selected_arm_all[alg_idx] = selected_arms
-        regrets_all[alg_idx] = np.array(best_reward) - np.array(rewards)
+        regrets_all[alg_idx] = best_reward - rewards
         arm_probs_all[alg_idx] = arm_prob
-        expected_rewards_all[alg_idx] = np.array(env_reward).dot(arm_probs_all[alg_idx, -1, :])
+        expected_rewards_all[alg_idx] = np.dot(env_reward, arm_probs_all[alg_idx, -1, :])
         time_cost[alg_idx] = time.time() - start_time
 
-    # After the simulation is done, increment the counter.
     with lock:
         counter.value += 1
         print(f"Job {simulation_idx} done, {counter.value}/{n_simulations} completed.")
